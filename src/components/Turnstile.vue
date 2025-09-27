ac8a955ff6dde5572b6b440e16f0266c475169aa
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-09-27 22:29:53 +0800

    [Feat] Add CAPTCHA component

---
+++ src/components/Turnstile.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const token = ref<string | null>(localStorage.getItem("turnstile_token"))
+function onVerifyCallback(res: string) {
+  token.value = res
+  localStorage.setItem("turnstile_token", res)
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="">
+      <div
+          class="cf-turnstile"
+          data-sitekey="0x4AAAAAAB3p7JESUfJ98K3S"
+          :data-callback="onVerifyCallback"
+      ></div>
+      <button type="submit">提交</button>
+    </form>
+  </div>
+</template>
