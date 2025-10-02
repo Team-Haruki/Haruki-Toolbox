2ae1add33fc36fae53200212e6b187bfd0b7c507
dnaroma <me@dna.moe>
2025-10-02 19:50:45 +0200

    [Refactor] Improve API request handling and error messaging in callApi function

--- src/components/users/data/api/call-api.ts
+++ src/components/users/data/api/call-api.ts
@@ -1,5 +1,7 @@
+import {toast} from "vue-sonner"
 import axios, {type AxiosRequestConfig, type Method} from "axios"
 import {useUserStore} from "@/components/users/data/store"
+import type { APIResponse } from "../types"
 
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_HARUKI_TOOLBOX_USER_BASE_URL,
@@ -11,10 +13,7 @@ export const apiClient = axios.create({
 apiClient.interceptors.request.use((config) => {
     const userStore = useUserStore()
     if (userStore.sessionToken) {
-        config.headers = {
-            ...config.headers,
-            Authorization: `${userStore.sessionToken}`,
-        }
+        config.headers.set('Authorization', userStore.sessionToken)
     }
     if (config.url && userStore.userId) {
         config.url = config.url.replace(`/api/user/{toolboxUserId}`, `/api/user/${userStore.userId}`)
@@ -36,8 +35,8 @@ export async function callApi<T = unknown>(
     method: Method = "GET",
     data?: any,
     config?: AxiosRequestConfig
-): Promise<T> {
-    const response = await apiClient.request<T>({
+): Promise<APIResponse<T>> {
+    const response = await apiClient.request<APIResponse<T>>({
         url: endpoint,
         method,
         data,
@@ -45,8 +44,17 @@ export async function callApi<T = unknown>(
     })
 
     if (isApiResponse<T>(response.data)) {
-        return response.data as unknown as T
+        if (response.data.status !== 200) {
+            toast.error(`API请求失败，状态码: ${response.data.status}，信息: ${response.data.message}`)
+            throw response.data
+        }
+        return response.data
     }
 
-    return response.data
+    toast.error(`API请求失败，无效的响应格式`)
+
+    throw {
+        status: 500,
+        message: "Invalid API response format",
+    }
 }
